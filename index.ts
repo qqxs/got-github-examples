eeb2b23b3f47d93061a649fd2842b378d0a65e65
ShineShao <xiaoshaoqq@gmail.com>
2021-06-12 18:14:11 +0800

    init

---
+++ index.ts
@@ -0,0 +1,31 @@
+// const got = require("got");
+
+import got from "got";
+import tar from "tar";
+import path from "path";
+import { Stream } from "stream";
+import { promisify } from "util";
+import { makeDir } from "./make-dir";
+
+const pipeline = promisify(Stream.pipeline);
+
+const root = path.resolve("data-fetch-app");
+
+async function downloadExample() {
+  await makeDir(root);
+  process.chdir(root);
+  return pipeline(
+    got.stream(
+      `https://codeload.github.com/qqxs/got-github-examples/tar.gz/master`
+    ),
+    tar.extract(
+      {
+        cwd: root,
+        strip: 3
+      },
+      [`got-github-examples-develop/examples/example01`]
+    )
+  );
+}
+
+downloadExample();
---
+++ make-dir.ts
@@ -0,0 +1,9 @@
+/** code from next.js/create-next-app */
+import fs from "fs";
+
+export function makeDir(
+  root: string,
+  options = { recursive: true }
+): Promise<string | undefined> {
+  return fs.promises.mkdir(root, options);
+}
